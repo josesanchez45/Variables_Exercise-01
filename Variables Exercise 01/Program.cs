c094ff992ca95698dab0937eb59f40d1ac6e6412
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-05 11:51:35 -0700

    fixed errors

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace Variables_Exercise_01
 {
@@ -12,22 +13,16 @@ namespace Variables_Exercise_01
             // TODO: Declare 3 constansts
 
 
-            const string myName= "Jose";
-            const char grade= 'A';
-            bool jobInTech= true;
+            const string myName = "Jose";
+            const char grade = 'A';
+            bool jobInTech = true;
             const int weeks = 9;
             double salary = 65000;
             decimal endSalary = 150000;
 
-            System.Console.WriteLine($"{myName} is enjoying class hopefuly passing with an {grade}.");
-            System.Console.WriteLine($" The class with take {weeks} and it is {jobInTech} that I want a coding job.");
-            System.Console.WriteLine($"Hopefully with a starting salary of a minimum {salary} but with potential to earn more than {endSalary}.);            // TODO: Declare a variable for each of the types above
-            // Make sure to use C# naming convention best practices 
-
-            // Done: Initialize each of the variables with a value
-
-            // Done: Write out each of the variables to the Console 
+            Console.WriteLine($"{myName} is enjoying class hopefuly passing with an {grade}.");
+            Console.WriteLine($" The class with take {weeks} and it is {jobInTech} that I want a coding job.");
+            Console.WriteLine($"Hopefully with a starting salary of a minimum {salary} but with potential to earn more than {endSalary}.");
         }
-
     }
 }
