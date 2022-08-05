86cfdfb371402cc8f0a2f1c5fa0f5b5d40cfe3e1
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-05 11:33:20 -0700

    initial commit

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -11,21 +11,22 @@ namespace Variables_Exercise_01
 
             // TODO: Declare 3 constansts
 
-            #region Types
-            // string
-            // char
-            // bool
-            // int
-            // double
-            // decimal
-            #endregion
 
-            // TODO: Declare a variable for each of the types above
+            const string myName= "Jose";
+            const char grade= 'A';
+            bool jobInTech= true;
+            const int weeks = 9;
+            double salary = 65000;
+            decimal endSalary = 150000;
+
+            System.Console.WriteLine($"{myName} is enjoying class hopefuly passing with an {grade}.");
+            System.Console.WriteLine($" The class with take {weeks} and it is {jobInTech} that I want a coding job.");
+            System.Console.WriteLine($"Hopefully with a starting salary of a minimum {salary} but with potential to earn more than {endSalary}.);            // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
 
-            // TODO: Initialize each of the variables with a value
+            // Done: Initialize each of the variables with a value
 
-            // TODO: Write out each of the variables to the Console 
+            // Done: Write out each of the variables to the Console 
         }
 
     }
